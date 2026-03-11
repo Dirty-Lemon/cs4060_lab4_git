34c4a1559fb0ed574b5be45dc1188c12e4592cd7
DESKTOP-C7ODU2E\KevinTerceros <tercerosofficial23@gmail.com>
2026-03-10 23:55:49 -0600

    Finished assignment 4

--- IntensityTransformsClass.py
+++ IntensityTransformsClass.py
@@ -1,15 +1,13 @@
 import cv2
+import numpy as np
 
 class IntensityTransforms():
     def __init__(self):
-        print("Intensity Transforms")
-        
-    def helloIntTrans(self):
-        print("Hello IT")
+        print("Intensity Transformations")
         
     # Read image
     def myReadImg(self, imgName):
-        imgPath = 'img/' + imgName
+        imgPath = 'img/' + imgName      # All images will be in the img folder
         print("Reading " + imgName + "...")
         return cv2.imread(imgPath)
     
@@ -18,6 +16,38 @@ class IntensityTransforms():
         cv2.imshow("Image", img)
         cv2.waitKey(0)
     
-    # Convert image to negative
-    def toNegImg(self, img):
-        print("Converting to negative image...")
+    # Takes a 2D array of images to show on one display
+    def showMultImg(self, imgsArray):
+        print("Loading multiple images...")
+        
+        imgNum = 1
+        for img in imgsArray:
+            title = 'Image ' + str(imgNum)
+            cv2.imshow(title, img)
+            imgNum += 1
+            
+        cv2.waitKey(0)
+    
+    # Performs negative transformation function on an image
+    # s = L - 1 - r
+    def negTransform(self, r):
+        print("Applying negative transformation to image...")
+        L = r.max()
+        s = L - 1 - r
+        return s
+    
+    # Performs the Power Law transformation function on an image
+    # s = cr^(gamma)
+    def powerLawTransform(self, imgData, gammaVal):
+        print("Applying power law transformation to image...")
+        r = imgData / 255
+        s = r ** gammaVal
+        return s
+    
+    # Performs the Log transformation function on an image
+    def logTransform (self, r):
+        print("Applying log transformation to image...")
+        r = r / 255
+        s = np.log(1 + r)
+        return s
+    
--- mainClass.py
+++ mainClass.py
@@ -2,14 +2,38 @@ from IntensityTransformsClass import *
 
 class main():
     i = IntensityTransforms()
-    i.helloIntTrans()
     
-    # Task 1 - Gray Scale Images
+    # Read initial image
     lenaImg = i.myReadImg('lena.tif')
     i.myShowImg(lenaImg)
-    i.toNegImg(lenaImg)
+    
+    # Task 1 part a) - negative transformation
+    negativeImg = i.negTransform(lenaImg)
+    i.myShowImg(negativeImg)
+    
+    # Task 1 part b) - power-law transformation
+    powCompressImg1 = i.powerLawTransform(lenaImg, 0.01)   # Perform gamma compression
+    powCompressImg2 = i.powerLawTransform(lenaImg, 0.05)   # Perform gamma compression
+    powCompressImg3 = i.powerLawTransform(lenaImg, 0.2)    # Perform gamma compression
+    powCompressImg4 = i.powerLawTransform(lenaImg, 0.5)    # Perform gamma compression
+    
+    # Show all gamma-compressed images on one chart
+    powCompressImgArray = [powCompressImg1, powCompressImg2, powCompressImg3, powCompressImg4]
+    i.showMultImg(powCompressImgArray)
+    
+    powExpandImg1 = i.powerLawTransform(lenaImg, 1.5)  # Perform gamma expansion
+    powExpandImg2 = i.powerLawTransform(lenaImg, 2)    # Perform gamma expansion
+    powExpandImg3 = i.powerLawTransform(lenaImg, 3)    # Perform gamma expansion
+    powExpandImg4 = i.powerLawTransform(lenaImg, 5)    # Perform gamma expansion
+    
+    # Show all gamma-expanded images on one chart
+    powExpandImgArray = [powExpandImg1, powExpandImg2, powExpandImg3, powExpandImg4]
+    i.showMultImg(powExpandImgArray)
     
     # Task 2 - Log Contrast Stretching
+    # What is FTspectrum.tif?
+    logImg = i.logTransform(lenaImg)
+    i.myShowImg(logImg)
     
 if __name__ == "__main__":
     main()
